f402f8f216d84f089f8ee95b80ee773f896237de
Ian King <ian.antking@gmail.com>
2018-08-29 20:53:28 +0100

    Completed the final kata function and test.

--- __tests__/kata7.getEmployerRole.test.js
+++ __tests__/kata7.getEmployerRole.test.js
@@ -3,5 +3,19 @@ const { getEmployerRole } = require('../src');
 describe('getEmployerRole', () => {
   it('returns the employee\'s role in the company', () => {
     
+    const employees = [{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Recommended Reading Assistant'
+    }]
+    
+    expect(getEmployerRole('Satti', employees)).toBe('Developer');
+    expect(getEmployerRole('Jenny', employees)).toBe('Sales Associate');
+    expect(getEmployerRole('Javid', employees)).toBe('Human Recommended Reading Assistant');
   });
 });
