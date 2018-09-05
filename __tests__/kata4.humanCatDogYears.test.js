16deb471efadd826f12cf52c6d2a07d97b435300
Ian King <ian.antking@gmail.com>
2018-09-05 18:40:50 +0100

    Fixed humanCatDogYears() to deal with being called 1 human year.

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -3,6 +3,7 @@ const { humanCatDogYears } = require('../src');
 // Look Ma, no handlebars!!!
 describe('humanCatDogYears', () =>{
     it('Returns an array of human cat and dog years for 3 human years', () => {
+        expect(humanCatDogYears(1)).toEqual([1,15,15]);
         expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
         expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
     });
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,7 +1,14 @@
 const humanCatDogYears = (number) => {
-    let catYears = (number - 2) * 4 + (15 + 9);
-    let dogYears = (number - 2) * 5 + (15 + 9);
+    let catYears = 0;
+    let dogYears = 0;
 
+    if (number === 1){
+        catYears = 15;
+        dogYears = 15;
+    }else{
+        catYears = (number - 2) * 4 + (15 + 9);
+        dogYears = (number - 2) * 5 + (15 + 9);
+    };
     return [number, catYears, dogYears];
     
 };
